200a9e368a0d5ef88d623ff466f2c9fcb41c9375
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-09-01 21:54:42 +0100

    new

--- main.js
+++ main.js
@@ -2,22 +2,6 @@ document.querySelector('.card-number-input').oninput = () =>{
     document.querySelector('.card_number').innerText = document.querySelector('.card-number-input').value;
 }
 
-$('#number').on('keyup', function(e){
-    // get value of the input field
-    var val = $(this).val();
-    var newval = '';
-    // write regex to remove any space
-    val = val.replace(/\s/g, '');
-    // iterate through each numver
-    for(var i = 0; i < val.length; i++) {
-        // add space if modulus of 4 is 0
-        if(i%4 == 0 && i > 0) newval = newval.concat(' ');
-        // concatenate the new value
-        newval = newval.concat(val[i]);
-    }
-    // update the final value in the html input
-    $(this).val(newval);
-});
 document.querySelector('.card-holder-input').oninput = () =>{
     document.querySelector('.card_holder_name').innerText = document.querySelector('.card-holder-input').value;
 }
